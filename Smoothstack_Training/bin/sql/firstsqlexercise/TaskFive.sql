bc49791b7eddee447904d6a8780cf8e7380a1ecc
Justin O'Brien <justin.obrien@smoothstack.com>
2020-04-22 23:08:56 -0600

    Update solution to task 5 of first SQL assignment

--- Smoothstack_Training/bin/sql/firstsqlexercise/TaskFive.sql
+++ Smoothstack_Training/bin/sql/firstsqlexercise/TaskFive.sql
@@ -1,5 +1,5 @@
 SELECT lb.branchName, count(*) 'Books currently loaned out'
 FROM tbl_library_branch lb
 JOIN tbl_book_loans bl ON lb.branchId = bl.branchId
-WHERE (bl.dateIn IS NULL OR bl.dateIn > CURDATE()) AND bl.dateOut <= CURDATE()
+WHERE (bl.dateIn IS NULL OR bl.dateIn > CURTIME()) AND bl.dateOut <= CURTIME()
 GROUP BY lb.branchId
