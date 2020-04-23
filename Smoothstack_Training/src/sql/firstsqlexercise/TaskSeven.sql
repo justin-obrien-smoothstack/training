da3e13843b74d405e5ab81c2cc70017e45ca55c6
Justin O'Brien <justin.obrien@smoothstack.com>
2020-04-22 22:09:39 -0600

    Finish task 7 of first SQL assignment

---
+++ Smoothstack_Training/src/sql/firstsqlexercise/TaskSeven.sql
@@ -0,0 +1,7 @@
+SELECT b.title, bc.noOfCopies
+FROM tbl_book b
+JOIN tbl_book_copies bc ON b.bookId = bc.bookId
+JOIN tbl_book_authors ba ON b.bookId = ba.bookId
+JOIN tbl_author a ON ba.authorId = a.authorId
+JOIN tbl_library_branch lb ON bc.branchId = lb.branchId
+WHERE a.authorName = 'Stephen King' AND lb.branchName = 'Central'
