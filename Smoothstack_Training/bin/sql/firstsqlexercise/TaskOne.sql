6eb06727dcfdde30da945a4f3358aef42d7b76f1
Justin O'Brien <justin.obrien@smoothstack.com>
2020-04-22 19:46:54 -0600

    Finish Task 2 of first SQL assignment
    Also slightly refine solution to Task 1

--- Smoothstack_Training/bin/sql/firstsqlexercise/TaskOne.sql
+++ Smoothstack_Training/bin/sql/firstsqlexercise/TaskOne.sql
@@ -1,4 +1,4 @@
-SELECT noOfCopies, lb.branchName, b.title FROM tbl_book_copies bc
-INNER JOIN tbl_library_branch lb ON bc.branchId = lb.branchId
-INNER JOIN tbl_book b ON bc.bookId = b.bookId
+SELECT bc.noOfCopies FROM tbl_book_copies bc
+JOIN tbl_library_branch lb ON bc.branchId = lb.branchId
+JOIN tbl_book b ON bc.bookId = b.bookId
 WHERE b.title = 'The Lost Tribe' AND lb.branchName = 'Sharpstown';
--- Smoothstack_Training/src/sql/firstsqlexercise/TaskOne.sql
+++ Smoothstack_Training/src/sql/firstsqlexercise/TaskOne.sql
@@ -1,4 +1,4 @@
-SELECT noOfCopies FROM tbl_book_copies bc
+SELECT bc.noOfCopies FROM tbl_book_copies bc
 JOIN tbl_library_branch lb ON bc.branchId = lb.branchId
 JOIN tbl_book b ON bc.bookId = b.bookId
 WHERE b.title = 'The Lost Tribe' AND lb.branchName = 'Sharpstown';
