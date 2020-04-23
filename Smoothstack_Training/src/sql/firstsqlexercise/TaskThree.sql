abbaed7cf4616210ae3a5e2032c9a51f226c2814
Justin O'Brien <justin.obrien@smoothstack.com>
2020-04-23 11:00:09 -0600

    Finish task 3 of first SQL assignment

--- Smoothstack_Training/src/sql/firstsqlexercise/TaskThree.sql
+++ Smoothstack_Training/src/sql/firstsqlexercise/TaskThree.sql
@@ -1,2 +1,6 @@
-SELECT b.name FROM tbl_borrower b
-JOIN tbl_book_loans bl ON b.cardNo = bl.cardNo
+SELECT name FROM tbl_borrower
+WHERE cardNo NOT IN (
+	SELECT b.cardNo FROM tbl_borrower b
+    JOIN tbl_book_loans bl ON b.cardNo = bl.cardNo
+    WHERE (bl.dateIn IS NULL OR bl.dateIn > CURTIME()) AND bl.dateOut <= CURTIME()
+)
