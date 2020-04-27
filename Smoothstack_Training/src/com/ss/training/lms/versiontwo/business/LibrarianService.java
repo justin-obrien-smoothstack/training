9d312e9a6f415cbc23b6554c06e1364397fbcc9d
Justin O'Brien <justin.obrien@smoothstack.com>
2020-04-27 15:59:11 -0600

    Remove unused methods from LibrarianService

--- Smoothstack_Training/src/com/ss/training/lms/versiontwo/business/LibrarianService.java
+++ Smoothstack_Training/src/com/ss/training/lms/versiontwo/business/LibrarianService.java
@@ -34,35 +34,6 @@ public class LibrarianService extends LMSService {
 	}
 
 	/**
-	 * Gets primary keys and names of all library branches
-	 * 
-	 * @return Primary keys (first row) and names (second row) of branches
-	 */
-	public Object[][] getBranchPksAndNames() {
-		return null; // placeholder
-	}
-
-	/**
-	 * Gets the name of a branch given its primary key
-	 * 
-	 * @param branchPk The branch's primary key
-	 * @return The branch's name
-	 */
-	public String getBranchName(int branchPk) {
-		return null; // placeholder
-	}
-
-	/**
-	 * Gets primary keys, titles, and authors of all books in the database
-	 * 
-	 * @return The primary keys of all books in the database (row 0) and strings
-	 *         containing their titles and authors (row 1)
-	 */
-	public Object[][] getAllBookPksTitlesAndAuthors() {
-		return null; // placeholder
-	}
-
-	/**
 	 * Updates the number of copies of a book at a library branch
 	 * 
 	 * @param branchPk          The primary key of the branch
@@ -99,5 +70,14 @@ public class LibrarianService extends LMSService {
 					thisCopies.setCopies(numCopies);
 			});
 		return "The number of book copies was successfully changed.";
+	}
+
+	/**
+	 * Gets primary keys and names of all library branches
+	 * 
+	 * @return Primary keys (first row) and names (second row) of branches
+	 */
+	public Object[][] getBranchPksAndNames() {
+		return null; // placeholder
 	}
 }
