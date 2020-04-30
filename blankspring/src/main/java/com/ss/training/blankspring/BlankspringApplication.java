a951c1135427f477a0bea97ed891cd2cc6ef4d23
Justin O'Brien <justin.obrien@smoothstack.com>
2020-04-30 15:53:38 -0600

    Push blank project generated with Spring Initializr

---
+++ blankspring/src/main/java/com/ss/training/blankspring/BlankspringApplication.java
@@ -0,0 +1,13 @@
+package com.ss.training.blankspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlankspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlankspringApplication.class, args);
+	}
+
+}
---
+++ blankspring/src/test/java/com/ss/training/blankspring/BlankspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.training.blankspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlankspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
