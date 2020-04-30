948f5d0ec63d1621c0a55a564cb9f4fa5bc15dae
Justin O'Brien <justin.obrien@smoothstack.com>
2020-04-29 21:49:33 -0600

    Initial commit of first webapp assignment

---
+++ webapp/src/main/java/com/gcit/servlet/MyServlet.java
@@ -0,0 +1,53 @@
+package com.gcit.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Validates login credentials sent via HTTP POST
+ */
+@WebServlet("/hi")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.print("<html><body>Hello there </body></html>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Map<String,String> userCredentials = new HashMap<String, String>();
+		String username = request.getParameter("username"), password = request.getParameter("password");
+		userCredentials.put("ThisIsNotMyPassword", "ThisIsMyUsername");
+		if(userCredentials.containsKey(password) && userCredentials.get(password).equals(username))
+			response.setStatus(202);
+		else
+			response.sendError(401);
+	}
+
+}
