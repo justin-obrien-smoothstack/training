be1401b3b19cde9fc8666bce5f27c4b2b1e5ad71
Justin O'Brien <justin.obrien@smoothstack.com>
2020-04-29 21:24:57 -0600

    Initial commit of webapp

---
+++ webapp/src/main/java/com/ss/training/firstwebappassignment/BasicPost.java
@@ -0,0 +1,55 @@
+package com.ss.training.firstwebappassignment;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet to validate username and password sent via POST
+ */
+@WebServlet("/login")
+public class BasicPost extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BasicPost() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.print("<html><body>Hello there </body></html>");
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Map<String, String> userCredentials = new HashMap<String, String>();
+		String username = request.getParameter("username"), password = request.getParameter("password");
+		userCredentials.put("ThisIsNotMyPassword", "ThisIsMyUsername");
+		if (userCredentials.containsKey(password) && userCredentials.get(password).equals(username))
+			response.setStatus(202);
+		else
+			response.sendError(401);
+	}
+
+}
