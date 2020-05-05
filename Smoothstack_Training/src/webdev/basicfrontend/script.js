635d0683366a14b13dd21d473b148086737ef5a2
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-05 12:37:27 -0600

    Work on script; add to index a bit

--- Smoothstack_Training/src/webdev/basicfrontend/script.js
+++ Smoothstack_Training/src/webdev/basicfrontend/script.js
@@ -1,5 +1,19 @@
-const books = [], numBooks = 20, numAuthors = 5, baseTitle = "Book #", baseAuthorName = "Author #";
-var i;
+const books = [], numBooks = 20, numAuthors = 5, baseTitle = "Book #",
+    baseAuthorName = "Author #", booksPerPage = 6, baseDivId = "result",
+    numPages = Math.ceil(numBooks / booksPerPage), pageNumId = "page-number";
+var i, currentPage = 1;
 for (i = 1; i <= numBooks; i++) {
-    books.push({ id: i, title: baseTitle + i, author: baseAuthorName + Math.ceil(numAuthors * Math.random()) });
+    books.push({
+        id: i, title: baseTitle + i, author: baseAuthorName +
+            Math.ceil(numAuthors * Math.random())
+    });
+}
+document.getElementById(pageNumId).innerHTML =
+    "Page " + currentPage + " of " + numPages;
+
+function previousPage() {
+    if (currentPage === 1) return;
+    for (i = 1; i < booksPerPage; i++) {
+        
+    }
 }
