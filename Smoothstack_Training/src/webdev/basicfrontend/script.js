288fac0710c49fd31ce1e798fa125a7eb14ed90e
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-05 14:03:22 -0600

    Finish basic frontend assignment, I think

--- Smoothstack_Training/src/webdev/basicfrontend/script.js
+++ Smoothstack_Training/src/webdev/basicfrontend/script.js
@@ -1,19 +1,46 @@
 const books = [], numBooks = 20, numAuthors = 5, baseTitle = "Book #",
     baseAuthorName = "Author #", booksPerPage = 6, baseDivId = "result",
-    numPages = Math.ceil(numBooks / booksPerPage), pageNumId = "page-number";
-var i, currentPage = 1;
+    numPages = Math.ceil(numBooks / booksPerPage),
+    nav = document.getElementById("nav"), body = document.getElementById("body"),
+    pageNum = document.getElementById("page-number");
+var i, currentPage = 1, div;
+function makeBookText(book) {
+    if (book === undefined) return "\n\n\n";
+    return "ID number: " + book.id + "\nTitle: " + book.title +
+        "\nAuthor: " + book.author;
+}
+
 for (i = 1; i <= numBooks; i++) {
     books.push({
         id: i, title: baseTitle + i, author: baseAuthorName +
             Math.ceil(numAuthors * Math.random())
     });
 }
-document.getElementById(pageNumId).innerHTML =
-    "Page " + currentPage + " of " + numPages;
+pageNum.innerText = "Page " + currentPage + " of " + numPages;
+body.insertBefore(document.createElement("hr"), nav)
+for (i = 0; i < booksPerPage; i++) {
+    div = document.createElement("div");
+    div.innerText = makeBookText(books[i]);
+    div.id = baseDivId + i;
+    body.insertBefore(div, nav);
+    body.insertBefore(document.createElement("hr"), nav);
+}
 
 function previousPage() {
     if (currentPage === 1) return;
-    for (i = 1; i < booksPerPage; i++) {
-        
+    var startingIndex = (currentPage - 2) * booksPerPage;
+    for (i = 0; i < booksPerPage; i++) {
+        document.getElementById(baseDivId + i).innerText =
+            makeBookText(books[startingIndex + i]);
     }
+    pageNum.innerText = "Page " + --currentPage + " of " + numPages;
+}
+function nextPage() {
+    if (currentPage === numPages) return;
+    var startingIndex = currentPage * booksPerPage;
+    for (i = 0; i < booksPerPage; i++) {
+        document.getElementById(baseDivId + i).innerText =
+            makeBookText(books[startingIndex + i]);
+    }
+    pageNum.innerText = "Page " + ++currentPage + " of " + numPages;
 }
